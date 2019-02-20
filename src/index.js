401f624f8aca9f57c7550ed07ba1e3965c1886b1
NardaC <nardac6@gmail.com>
2019-02-20 12:40:05 -0500

    ocultando secciones

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-/* Acá va tu código */
+//secciones//
+const home = document.getElementById("home");
+const escribirMsj = document.getElementById("escribirMsj");
+const msjCifrado = document.getElementById("msjCifrado");
+const msjDecifrado = document.getElementById("msjDecifrado");
+escribirMsj.style.display= "none";
+msjCifrado.style.display= "none";
+msjDecifrado.style.display= "none";
+
+function btnComenzar(){
+    escribirMsj.style.display = "block",
+    home.style.display = "none";
+    msjCifrado.style.display= "none";
+    msjDecifrado.style.display= "none";
+} 
+
+function btnCifrar(){
+    msjCifrado.style.display= "block";
+    msjDecifrado.style.display= "none";
+}
