0451c773bfa579c2df63d3f57e3b96b78e8a1928
NardaC <nardac6@gmail.com>
2019-02-21 12:17:02 -0500

    nuevo codigo para ocultar pantallas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 //secciones//
-const home = document.getElementById("home");
-const escribirMsj = document.getElementById("escribirMsj");
-const msjCifrado = document.getElementById("msjCifrado");
-const msjDecifrado = document.getElementById("msjDecifrado");
-escribirMsj.style.display= "none";
-msjCifrado.style.display= "none";
-msjDecifrado.style.display= "none";
-
-function btnComenzar(){
-    escribirMsj.style.display = "block",
-    home.style.display = "none";
-    msjCifrado.style.display= "none";
-    msjDecifrado.style.display= "none";
-} 
-
-function btnCifrar(){
-    msjCifrado.style.display= "block";
-    msjDecifrado.style.display= "none";
+window.onload = () => {
+    document.getElementById("escribirMsj").style.display="none";
+    document.getElementById("msjCifrado").style.display="none";
+    document.getElementById("msjDecifrado").style.display="none";  
 }
+
+document.getElementById("btnComenzar").addEventListener("click",() => {
+    document.getElementById("home").style.display="none";
+    document.getElementById("msjCifrado").style.display="none";
+    document.getElementById("msjDecifrado").style.display="none";
+    document.getElementById("escribirMsj").style.display="block";
+});
+
+document.getElementById("btnCifrar").addEventListener("click",() => {
+    //document.getElementById("home").style.display="none";
+    document.getElementById("msjCifrado").style.display="block";
+    document.getElementById("msjDecifrado").style.display="none";
+    document.getElementById("escribirMsj").style.display="block";
+});
+
+//let cifrado = document.getElementById("btnCifrar");
