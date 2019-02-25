47418eaf42275556141a053a95c2203c795b261d
NardaC <nardac6@gmail.com>
2019-02-25 12:57:45 -0500

    cifrando1

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,31 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+    //creamos una varialble que sera nuestro resultado final
+    const resultado = '';
+    //usamos el bucle for para repetir por todo el string
+    for(let i= 0; i < string.length; i ++){
+      //para mayusculas
+      if(65 <= string.charCodeAt(i) && string.charCodeAt(i) <= 90){
+        //obtener el ascci del string
+        let ascci = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+        //obtener e imprimir el mensaje cifrado obtenido del ascci
+        let nuevoMsj = String.fromCharCode(ascci);
+        document.write(nuevoMsj);
+
+      }
+      //para minusculas 
+      else if (97<= string.charCodeAt(i) && string.charCodeAt(i) <= 122){
+      let ascci = (string.charCodeAt(i) - 97 + offset) % 26 + 97;
+      let nuevoMsj = String.fromCharCode(ascci);
+      document.write(nuevoMsj);
+    }
+    
+    // para espacio 
+    else if (string.charCodeAt(i) === 32){
+    document.write(" ");
   }
-};
+      resultado += nuevoMsj;
+  }
+  return reultado;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
-//secciones//
+//ocultar las demas paginas y mostrar solo la primera seccion home al cargar
 window.onload = () => {
     document.getElementById("escribirMsj").style.display="none";
     document.getElementById("msjCifrado").style.display="none";
     document.getElementById("msjDecifrado").style.display="none";  
 }
-
+//al hacer click al boton comenzar se esconde la seccion home y muestra las demas
 document.getElementById("btnComenzar").addEventListener("click",() => {
     document.getElementById("home").style.display="none";
     document.getElementById("msjCifrado").style.display="none";
     document.getElementById("msjDecifrado").style.display="none";
     document.getElementById("escribirMsj").style.display="block";
 });
+//creo 2 vriables  para los botones de cifrar y decifrar
+let cifrar = document.getElementById('btnCifrar');
+let decifrar = document.getElementById('btnDecifrar');
+//ante el evento de hacer clic cifra y decifra
+cifrar.addEventListener('click',encode);
+decifrar.addEventListener('click',decode);
 
-document.getElementById("btnCifrar").addEventListener("click",() => {
-    //document.getElementById("home").style.display="none";
-    document.getElementById("msjCifrado").style.display="block";
-    document.getElementById("msjDecifrado").style.display="none";
-    document.getElementById("escribirMsj").style.display="block";
-});
+//creo una funcion para dentro de esta llamar a la función cipher.encode y pasarle argumentos
+encode () => {
+    const string = document.getElementById('msj').value;
+    const offset = document.getElementById('clave').value;
+    const resultado = cipher.encode(offset,string);
 
-//let cifrado = document.getElementById("btnCifrar");
+    document.getElementById('msjCifrado').innerHTML = resultado;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
