07c842f8cd56adf8ff79266753439caca69da792
NardaC <nardac6@gmail.com>
2019-02-26 21:09:31 -0500

    descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -2,18 +2,19 @@ window.cipher = {
   encode: (offset,string) => {
     //creamos una varialble que sera nuestro resultado final
     let resultado = '';
-    offset = offset % 26;
+    let offset1 = parseInt(offset);
+    //let prueba = parseInt('offset);
     //usamos el bucle for para repetir por todo el string
     for(let i= 0; i < string.length; i ++){
       //para mayusculas
       if(65 <= string.charCodeAt(i) && string.charCodeAt(i) <= 90){
         //obtener el ascci del string
-        let ascci = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+        let ascci = (string.charCodeAt(i) - 65 + offset1) % 26 + 65;
         //obtener e imprimir el mensaje cifrado obtenido del ascci
         let nuevoMsj = String.fromCharCode(ascci);
         resultado += nuevoMsj;
       }else if (97<= string.charCodeAt(i) && string.charCodeAt(i) <= 122){ //para minusculas
-        let ascci = (string.charCodeAt(i) - 97 + offset) % 26 + 97;
+        let ascci = (string.charCodeAt(i) - 97 + offset1) % 26 + 97;
         let nuevoMsj = String.fromCharCode(ascci);
         resultado += nuevoMsj;
       }else if (string.charCodeAt(i) === 32) {resultado += " "};
@@ -22,4 +23,28 @@ window.cipher = {
   //resultado += nuevoMsj;
   return resultado; 
   },
-}
+  decode: (offset,string) => {
+    //creamos una varialble que sera nuestro resultado final
+    let resultado = '';
+    let offset2 = parseInt(offset);
+    //let prueba = parseInt('offset);
+    //usamos el bucle for para repetir por todo el string
+    for(let i= 0; i < string.length; i ++){
+      //para mayusculas
+      if(65 <= string.charCodeAt(i) && string.charCodeAt(i) <= 90){
+        //obtener el ascci del string
+        let ascci = (string.charCodeAt(i) - 65 - offset2) % 26 + 65;
+        //obtener e imprimir el mensaje cifrado obtenido del ascci
+        let nuevoMsj = String.fromCharCode(ascci);
+        resultado += nuevoMsj;
+      }else if (97<= string.charCodeAt(i) && string.charCodeAt(i) <= 122){ //para minusculas
+        let ascci = (string.charCodeAt(i) - 97 - offset2) % 26 + 97;
+        let nuevoMsj = String.fromCharCode(ascci);
+        resultado += nuevoMsj;
+      }else if (string.charCodeAt(i) === 32) {resultado += " "};
+    } //para espacio
+  
+  //resultado += nuevoMsj;
+  return resultado; 
+  },
+};
